70b39a45581e43d2efe68b1071f5bef25a6357e3
Sebastian <salpizar@outcoding.com>
2019-11-14 19:28:13 -0600

    Added editor

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,7 @@
 /*  www  .  jav a2s. com*/
-import java.awt.BorderLayout;
+import java.awt.*;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JTextPane;
+import javax.swing.*;
 import javax.swing.text.AttributeSet;
 import javax.swing.text.Element;
 import javax.swing.text.MutableAttributeSet;
@@ -15,24 +13,46 @@ public class Main {
 
     public static void main(String[] args) {
         JFrame frame = new TextStyleTestFrame();
-        frame.setSize(300, 300);
+        frame.setSize(600, 800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.setResizable(false);
     }
 }
 
 class TextStyleTestFrame extends JFrame {
-    JButton btnStyle = new JButton("Bold");
-    JTextPane textPane = new JTextPane();
+    private JButton button1 = new JButton("Bold");
+    private JButton button2 = new JButton("Italic");
+    private JButton button3 = new JButton("Underline");
+    private JTextPane textPane = new JTextPane();
 
     public TextStyleTestFrame() {
+        button1.setPreferredSize(new Dimension(200, 25));
+        button2.setPreferredSize(new Dimension(200, 25));
+        button3.setPreferredSize(new Dimension(200, 25));
         textPane.setText("this is a test.");
+        Font font1 = new Font("SansSerif", Font.PLAIN, 20);
+        textPane.setFont(font1);
         add(textPane, BorderLayout.CENTER);
-        add(btnStyle, BorderLayout.NORTH);
-        btnStyle.addActionListener(e ->changeStyle());
+
+        JPanel panel = new JPanel();
+
+        panel.setLayout(new BorderLayout());
+        panel.add(button1, BorderLayout.LINE_START);
+        panel.add(button3, BorderLayout.CENTER);
+        panel.add(button2, BorderLayout.LINE_END);
+        add(panel, BorderLayout.BEFORE_FIRST_LINE);
+
+
+        button1.addActionListener(e -> changeStyleBold());
+        button2.addActionListener(e -> changeStyleItalic());
+        button3.addActionListener(e -> changeStyleUnderline());
+
+
     }
-    private void changeStyle() {
+
+    private void changeStyleBold() {
         StyledDocument doc = (StyledDocument) textPane.getDocument();
         int selectionEnd = textPane.getSelectionEnd();
         int selectionStart = textPane.getSelectionStart();
@@ -47,6 +67,42 @@ class TextStyleTestFrame extends JFrame {
         doc.setCharacterAttributes(selectionStart, textPane.getSelectedText()
                 .length(), asNew, true);
         String text = (StyleConstants.isBold(as) ? "Cancel Bold" : "Bold");
-        btnStyle.setText(text);
+        button1.setText(text);
+    }
+
+    private void changeStyleItalic() {
+        StyledDocument doc = (StyledDocument) textPane.getDocument();
+        int selectionEnd = textPane.getSelectionEnd();
+        int selectionStart = textPane.getSelectionStart();
+        if (selectionStart == selectionEnd) {
+            return;
+        }
+        Element element = doc.getCharacterElement(selectionStart);
+        AttributeSet as = element.getAttributes();
+
+        MutableAttributeSet asNew = new SimpleAttributeSet(as.copyAttributes());
+        StyleConstants.setItalic(asNew, !StyleConstants.isItalic(as));
+        doc.setCharacterAttributes(selectionStart, textPane.getSelectedText()
+                .length(), asNew, true);
+        String text = (StyleConstants.isBold(as) ? "Cancel Italic" : "Italic");
+        button2.setText(text);
+    }
+
+    private void changeStyleUnderline() {
+        StyledDocument doc = (StyledDocument) textPane.getDocument();
+        int selectionEnd = textPane.getSelectionEnd();
+        int selectionStart = textPane.getSelectionStart();
+        if (selectionStart == selectionEnd) {
+            return;
+        }
+        Element element = doc.getCharacterElement(selectionStart);
+        AttributeSet as = element.getAttributes();
+
+        MutableAttributeSet asNew = new SimpleAttributeSet(as.copyAttributes());
+        StyleConstants.setUnderline(asNew, !StyleConstants.isUnderline(as));
+        doc.setCharacterAttributes(selectionStart, textPane.getSelectedText()
+                .length(), asNew, true);
+        String text = (StyleConstants.isUnderline(as) ? "Cancel Underline" : "Underline");
+        button3.setText(text);
     }
 }
